4d9baea65d8e8055bc312cefede622840888d472
ASRSQ <a.s.rwar@gmail.com>
2025-03-10 17:10:17 -0300

    a

--- test-db.php
+++ test-db.php
@@ -1,10 +1,10 @@
 <?php
-$host = '172.18.0.3';
+$host = 'interspire_db';
 $user = 'root';
-$pass = 'root';  // Se você definiu uma senha, insira aqui
+$pass = 'root';  // Use a senha correta
 $dbname = 'interspire_db';
 
-$mysqli = new mysqli($host, $user, $pass, $dbname);
+$mysqli = new mysqli($host, $user, $pass, $dbname, 3306, null, MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT);
 
 if ($mysqli->connect_errno) {
     die("Erro de conexão ({$mysqli->connect_errno}): {$mysqli->connect_error}");
